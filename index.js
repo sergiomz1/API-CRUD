a03f3fb1e236d934bcc765e5e911c8983b7a253a
sergiomz1 <85529375+sergiomz1@users.noreply.github.com>
2024-09-12 17:23:21 -0500

    conexiones

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // import { Express } from 'express'
 
-const express = require ('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 app.listen(3000, () => {
-  console.log("server corriendo en le puerto 3000")
-})
+  console.log("server corriendo en le puerto 3000");
+});
 
-app.get('/', (req,res) =>{
-  res.send("hola desde node Api")
-})
+app.get("/", (req, res) => {
+  res.send("hola desde node Api desde vs codeeee");
+});
+mongoose
+  .connect(
+    "mongodb+srv://sergiom8alexanderd8:5IogQxLWciiFJrTu@cluster0.foawt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("OK");
+  })
+  .catch(() => {
+    console.log("fallo");
+  });
